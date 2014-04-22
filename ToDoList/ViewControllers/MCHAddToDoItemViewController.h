894befcfbcfdd4a3e86b424f2aafed9451f749d1
Jalen <jiayuewang228@gmail.com>
2014-04-22 22:30:34 +0800

    sub-classing the view controller

---
+++ ToDoList/ViewControllers/MCHAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCHAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by wangjiay on 4/22/14.
+//  Copyright (c) 2014 zhaohengtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCHAddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/ViewControllers/MCHToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCHToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by wangjiay on 4/22/14.
+//  Copyright (c) 2014 zhaohengtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCHToDoListTableViewController : UITableViewController
+
+@end
