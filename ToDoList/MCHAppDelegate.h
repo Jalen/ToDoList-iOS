22a7479aef87410f895f509b18982682a24d9f0c
Jalen <jiayuewang228@gmail.com>
2014-04-22 13:59:09 +0800

    Initial Commit

---
+++ ToDoList/MCHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCHAppDelegate.h
+//  ToDoList
+//
+//  Created by wangjiay on 4/22/14.
+//  Copyright (c) 2014 zhaohengtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by wangjiay on 4/22/14.
+//  Copyright (c) 2014 zhaohengtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCHAppDelegate class]));
+    }
+}
